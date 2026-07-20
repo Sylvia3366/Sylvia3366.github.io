e95079f642afba40b9199d9b9a2d33b62d608ba3
Sylvia3366 <linsylvia902@gmail.com>
2026-07-20 09:18:18 -0400

    upper border links

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistPixel.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col font-sans">{children}</body>
+    <html lang="en">
+      <body className="min-h-full flex flex-col font-sans">
+        <Navbar /> 
+        {children}
+      </body>
     </html>
+  );
+}
+
+export function Navbar() {
+  return (
+    <nav className="flex items-center justify-between bg-neutral-800 p-4 text-white">
+      {/* Logo or Name */}
+      <div className="font-bold">Sylvia Lin</div>
+      
+      {/* Navigation Links */}
+      <ul className="flex gap-6">
+        <li><a href="/" className="hover:text-gray-300">Home</a></li>
+        <li><a href="https://www.instagram.com/ainuoqiu/" className="hover:text-gray-300">Instagram</a></li>
+        <li><a href="https://github.com/Sylvia3366" target="_blank" className="hover:text-gray-300">GitHub</a></li>
+        <li><a href="https://linkedin.com" target="_blank" className="hover:text-gray-300">LinkedIn</a></li>
+      </ul>
+    </nav>
   );
 }
